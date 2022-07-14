c3294293438dd6ca18182a8379bc72087b4fa297
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-07-15 04:58:36 +0900

    Translation: ja_JP (日本語)
    Added Japanese translation. / 日本語の翻訳を追加しました。
    I'm Japanese, but my Japanese is poor.

---
+++ Source/Resource/Translation/apd_ja_JP.ts
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>ApdApplication</name>
+    <message>
+        <source>Settings format has changed a bit and needs to be reconfigured.</source>
+        <translation>設定ファイルのフォーマットが変更されたので、再設定が必要です。</translation>
+    </message>
+    <message>
+        <source>Hello, welcome to %1!
+
+This seems to be your first time using this program.
+Let&apos;s configure something together.</source>
+        <translation>%1 へようこそ！
+        
+このプログラムを使うのは初めてのようですね。
+初期設定を行いましょう！</translation>
+    </message>
+    <message>
+        <source>Do you want this program to launch when the system starts?
+
+If you frequently use AirPods with this computer, it is recommended that you click &quot;Yes&quot;.</source>
+        <translation>コンピューターの起動時にプログラムを自動的に起動しますか？
+
+このコンピュータでAirPodsを頻繁に使用する場合は、「はい」をクリックすることを推奨します。</translation>
+    </message>
+    <message>
+        <source>Do you want to enable the &quot;low audio latency&quot; feature?
+
+%1</source>
+        <translation>「低遅延モード」を有効にしますか？
+
+%1</translation>
+    </message>
+    <message>
+        <source>Great! Everything is ready!
+
+Enjoy it all.</source>
+        <translation>全ての準備が完了しました！
+
+快適なオーディオライフをお楽しみください！</translation>
+    </message>
+    <message>
+        <source>You can find me in the system tray</source>
+        <translation>タスクバーの通知領域に常駐しています</translation>
+    </message>
+    <message>
+        <source>Click the icon to view battery information, right-click to customize settings or quit.</source>
+        <translation>アイコンをクリックするとバッテリー情報が表示され、右クリックで設定をカスタマイズしたり、完全に終了することができます。</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadWindow</name>
+    <message>
+        <source>Download new version</source>
+        <translation>新しいバージョンをダウンロード</translation>
+    </message>
+    <message>
+        <source>Download Manually</source>
+        <translation>自分でダウンロード</translation>
+    </message>
+    <message>
+        <source>If the download is slow or fails, you can:</source>
+        <translation>ダウンロードが遅かったり失敗する場合、以下のことをお試しください:</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::DownloadWindow</name>
+    <message>
+        <source>Oops, an error occurred during the automatic update.
+Please download and install the new version manually.</source>
+        <translation>おっと！自動更新中にエラーが起きてしまいました。
+お手数をおかけしますが、新しいバージョンを手動でダウンロードして、インストールしてください。</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::MainWindow</name>
+    <message>
+        <source>Change log:</source>
+        <translation>変更履歴:</translation>
+    </message>
+    <message>
+        <source>Hey! I found a new version available!
+
+Current version: %1
+Latest version: %2%3</source>
+        <translation>新しいバージョンが利用可能です。
+
+現在のバージョン: %1
+新しいバージョン: %2%3</translation>
+    </message>
+    <message>
+        <source>Bind to AirPods</source>
+        <translation>AirPodsに結びつける</translation>
+    </message>
+    <message>
+        <source>Please select your AirPods device below.</source>
+        <translation>AirPodsを以下から選んでください。</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::SettingsWindow</name>
+    <message>
+        <source>|</source>
+        <extracomment>To credit translators, you can leave your name here if you wish. (Sorted by time added, separated by &quot;|&quot; character, only single &quot;|&quot; for empty)</extracomment>
+        <translation>Ryuta Iwakura</translation>
+    </message>
+    <message>
+        <source>Translation Contributors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Third-Party Libraries:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Gui::TrayIcon</name>
+    <message>
+        <source>Left</source>
+        <translation>左</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>右</translation>
+    </message>
+    <message>
+        <source>Case</source>
+        <translation>ケース</translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <translation>充電中</translation>
+    </message>
+    <message>
+        <source>New version available!</source>
+        <translation>新しいバージョンが利用可能です！</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>情報</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>終了</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>AirPodsDesktop information</source>
+        <translation>AirPodsDesktop について</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>Update now</source>
+        <translation>今すぐアップデート</translation>
+    </message>
+    <message>
+        <source>Skip this version</source>
+        <translation>このバージョンをスキップ</translation>
+    </message>
+    <message>
+        <source>View release</source>
+        <translation>リリースを見る</translation>
+    </message>
+    <message>
+        <source>Remind me later</source>
+        <translation>後で通知</translation>
+    </message>
+    <message>
+        <source>No paired device found.
+You need to pair your AirPods in Windows Bluetooth Settings first.</source>
+        <translation>ペアリングされたデバイスが見つかりません。
+先にWindowsのBluetooth設定からAirPodsとペアリングする必要があります。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>It fixes short audio playback problems, but may increase battery consumption.</source>
+        <translation>音声の遅延を改善しますが、電池消費が増加する場合があります。</translation>
+    </message>
+    <message>
+        <source>It automatically pauses or resumes media when your AirPods are taken out or put in your ears.</source>
+        <translation>AirPodsを外すと自動的に一時停止し、着けると再生を再開します。</translation>
+    </message>
+    <message>
+        <source>Unavailable</source>
+        <translation>利用できません</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>切断されました</translation>
+    </message>
+    <message>
+        <source>Waiting for Binding</source>
+        <translation>結びつけられるのを待っています</translation>
+    </message>
+</context>
+<context>
+    <name>SelectWindow</name>
+    <message>
+        <source>Select</source>
+        <translation>選択</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>AirPodsDesktop Settings</source>
+        <translation>AirPodsDesktop の設定</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>一般</translation>
+    </message>
+    <message>
+        <source>Open logs directory</source>
+        <translation>ログファイルのフォルダーを開く</translation>
+    </message>
+    <message>
+        <source>Bluetooth maximum receiving range</source>
+        <translation>Bluetoothの最大受信距離</translation>
+    </message>
+    <message>
+        <source>Unbind AirPods</source>
+        <translation>AirPodsとの結びつけを解除する</translation>
+    </message>
+    <message>
+        <source>Launch when system starts</source>
+        <translation>コンピュータの起動時に自動で起動</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>言語</translation>
+    </message>
+    <message>
+        <source>Visual</source>
+        <translation>見た目</translation>
+    </message>
+    <message>
+        <source>Display battery on tray icon</source>
+        <translation>通知アイコンに電池残量を表示する</translation>
+    </message>
+    <message>
+        <source>Disable</source>
+        <translation>無効</translation>
+    </message>
+    <message>
+        <source>When low battery</source>
+        <translation>バッテリー残量が少ない時のみ</translation>
+    </message>
+    <message>
+        <source>Always</source>
+        <translation>常に表示</translation>
+    </message>
+    <message>
+        <source>Features</source>
+        <translation>機能</translation>
+    </message>
+    <message>
+        <source>Automatic ear detection</source>
+        <translation>自動耳検出</translation>
+    </message>
+    <message>
+        <source>Low audio latency</source>
+        <translation>低遅延モード</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>情報</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open source &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;repository&lt;/span&gt;&lt;/a&gt; and licensed under &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;GPLv3&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;&lt;p&gt;オープンソース&lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;リポジトリ&lt;/span&gt;&lt;/a&gt;で、&lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;GPLv3&lt;/span&gt;&lt;/a&gt;の下にライセンスされています。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Copyright © 2021-2022 SpriteOvO. All rights reserved.</source>
+        <translation>Copyright © 2021-2022 SpriteOvO. All rights reserved.</translation>
+    </message>
+    <message>
+        <source>Display battery on taskbar</source>
+        <translation type="unfinished">タスクバーに電池残量を表示</translation>
+    </message>
+    <message>
+        <source>As text</source>
+        <translation type="unfinished">文字で表示</translation>
+    </message>
+    <message>
+        <source>As icon</source>
+        <translation type="unfinished">アイコンで表示</translation>
+    </message>
+</context>
+</TS>
