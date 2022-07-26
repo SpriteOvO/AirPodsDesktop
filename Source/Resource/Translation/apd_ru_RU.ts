abaf9ce2558cbd85794967b53090f304a60983aa
Sprite <SpriteOvO@gmail.com>
2022-07-26 23:41:30 +0800

    Add Russian translation (#36)

--- Source/Resource/Translation/apd_ru_RU.ts
+++ Source/Resource/Translation/apd_ru_RU.ts
@@ -5,55 +5,64 @@
     <name>ApdApplication</name>
     <message>
         <source>Settings format has changed a bit and needs to be reconfigured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Формат настройки был изменен и нуждается в перенастройке.</translation>
     </message>
     <message>
         <source>Hello, welcome to %1!
 
 This seems to be your first time using this program.
 Let&apos;s configure something together.</source>
-        <translation type="unfinished"></translation>
+        <translation>Привет, добро пожаловать в %1!
+
+Похоже, вы впервые используете эту программу.
+Давайте настроим программу под вас.</translation>
     </message>
     <message>
         <source>Do you want this program to launch when the system starts?
 
 If you frequently use AirPods with this computer, it is recommended that you click &quot;Yes&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы хотите, чтобы эта программа запускалась при запуске системы?
+
+Если вы часто используете AirPods на этом компьютере, рекомендуется нажать кнопку &quot;Yes&quot;.</translation>
     </message>
     <message>
         <source>Do you want to enable the &quot;low audio latency&quot; feature?
 
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы хотите включить функцию &quot;низкой задержки звука&quot;?
+
+%1</translation>
     </message>
     <message>
         <source>Great! Everything is ready!
 
 Enjoy it all.</source>
-        <translation type="unfinished"></translation>
+        <translation>Отлично! Все готово!
+
+Приятного использования программы.</translation>
     </message>
     <message>
         <source>You can find me in the system tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете найти меня в трее</translation>
     </message>
     <message>
         <source>Click the icon to view battery information, right-click to customize settings or quit.</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите на значок, чтобы просмотреть заряд аккумулятора, нажмите правой кнопкой мыши, чтобы настроить параметры, или выйти.</translation>
     </message>
 </context>
 <context>
     <name>DownloadWindow</name>
     <message>
         <source>Download new version</source>
-        <translation type="unfinished"></translation>
+        <translation>Скачать новую версию</translation>
     </message>
     <message>
         <source>Download Manually</source>
-        <translation type="unfinished"></translation>
+        <translation>Скачать вручную</translation>
     </message>
     <message>
         <source>If the download is slow or fails, you can:</source>
-        <translation type="unfinished"></translation>
+        <translation>Если загрузка происходит медленно или с ошибками, вы можете:</translation>
     </message>
 </context>
 <context>
@@ -61,29 +70,33 @@ Enjoy it all.</source>
     <message>
         <source>Oops, an error occurred during the automatic update.
 Please download and install the new version manually.</source>
-        <translation type="unfinished"></translation>
+        <translation>Упс, во время автоматического обновления произошла ошибка.
+Пожалуйста, загрузите и установите новую версию вручную.</translation>
     </message>
 </context>
 <context>
     <name>Gui::MainWindow</name>
     <message>
         <source>Change log:</source>
-        <translation type="unfinished"></translation>
+        <translation>Список изменений:</translation>
     </message>
     <message>
         <source>Hey! I found a new version available!
 
 Current version: %1
 Latest version: %2%3</source>
-        <translation type="unfinished"></translation>
+        <translation>Найдена новая версия!
+
+Текущая версия:%1
+Последняя версия:%2%3</translation>
     </message>
     <message>
         <source>Bind to AirPods</source>
-        <translation type="unfinished"></translation>
+        <translation>Привязка к AirPods</translation>
     </message>
     <message>
         <source>Please select your AirPods device below.</source>
-        <translation type="unfinished"></translation>
+        <translation>Пожалуйста, выберите свои AirPods ниже.</translation>
     </message>
 </context>
 <context>
@@ -91,198 +104,199 @@ Latest version: %2%3</source>
     <message>
         <source>|</source>
         <extracomment>To credit translators, you can leave your name here if you wish. (Sorted by time added, separated by &quot;|&quot; character, only single &quot;|&quot; for empty)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Translation Contributors:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Third-Party Libraries:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>Gui::TrayIcon</name>
     <message>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Левый</translation>
     </message>
     <message>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Правый</translation>
     </message>
     <message>
         <source>Case</source>
-        <translation type="unfinished"></translation>
+        <translation>Кейс</translation>
     </message>
     <message>
         <source>charging</source>
-        <translation type="unfinished"></translation>
+        <translation>зарядка</translation>
     </message>
     <message>
         <source>New version available!</source>
-        <translation type="unfinished"></translation>
+        <translation>Новая версия доступна!</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Выйти</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>AirPodsDesktop information</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация о AirPodsDesktop</translation>
     </message>
 </context>
 <context>
     <name>QMessageBox</name>
     <message>
         <source>Update now</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновить сейчас</translation>
     </message>
     <message>
         <source>Skip this version</source>
-        <translation type="unfinished"></translation>
+        <translation>Пропустить эту версию</translation>
     </message>
     <message>
         <source>View release</source>
-        <translation type="unfinished"></translation>
+        <translation>Посмотреть релиз</translation>
     </message>
     <message>
         <source>Remind me later</source>
-        <translation type="unfinished"></translation>
+        <translation>Напомнить позже</translation>
     </message>
     <message>
         <source>No paired device found.
 You need to pair your AirPods in Windows Bluetooth Settings first.</source>
-        <translation type="unfinished"></translation>
+        <translation>Сопряженное устройство не найдено.
+Сначала вам необходимо выполнить сопряжение AirPods в настройках Bluetooth Windows.</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>It fixes short audio playback problems, but may increase battery consumption.</source>
-        <translation type="unfinished"></translation>
+        <translation>Это устраняет проблемы с воспроизведением звука, но может увеличить расход батареи наушников.</translation>
     </message>
     <message>
         <source>It automatically pauses or resumes media when your AirPods are taken out or put in your ears.</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматическая приостановка или возобновление воспроизведения мультимедиа, когда вы снимаете или надеваете AirPods.</translation>
     </message>
     <message>
         <source>Unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Недоступно</translation>
     </message>
     <message>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Отключено</translation>
     </message>
     <message>
         <source>Waiting for Binding</source>
-        <translation type="unfinished"></translation>
+        <translation>Ожидание привязки</translation>
     </message>
 </context>
 <context>
     <name>SelectWindow</name>
     <message>
         <source>Select</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать</translation>
     </message>
 </context>
 <context>
     <name>SettingsWindow</name>
     <message>
         <source>AirPodsDesktop Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки AirPodsDesktop</translation>
     </message>
     <message>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Основные</translation>
     </message>
     <message>
         <source>Open logs directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Открыть папку логов</translation>
     </message>
     <message>
         <source>Bluetooth maximum receiving range</source>
-        <translation type="unfinished"></translation>
+        <translation>Максимальная дальность приема Bluetooth</translation>
     </message>
     <message>
         <source>Unbind AirPods</source>
-        <translation type="unfinished"></translation>
+        <translation>Отвязать AirPods</translation>
     </message>
     <message>
         <source>Launch when system starts</source>
-        <translation type="unfinished"></translation>
+        <translation>Запуск вместе с системой</translation>
     </message>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Язык</translation>
     </message>
     <message>
         <source>Visual</source>
-        <translation type="unfinished"></translation>
+        <translation>Визуальные</translation>
     </message>
     <message>
         <source>Display battery on taskbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Отображение заряда батареи на панели задач</translation>
     </message>
     <message>
         <source>Disable</source>
-        <translation type="unfinished"></translation>
+        <translation>Выключить</translation>
     </message>
     <message>
         <source>When low battery</source>
-        <translation type="unfinished"></translation>
+        <translation>При низком заряде батареи</translation>
     </message>
     <message>
         <source>Always</source>
-        <translation type="unfinished"></translation>
+        <translation>Всегда</translation>
     </message>
     <message>
         <source>As text</source>
-        <translation type="unfinished"></translation>
+        <translation>В виде текста</translation>
     </message>
     <message>
         <source>As icon</source>
-        <translation type="unfinished"></translation>
+        <translation>В виде иконки</translation>
     </message>
     <message>
         <source>Display battery on tray icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Отображение значка батареи в трее</translation>
     </message>
     <message>
         <source>Features</source>
-        <translation type="unfinished"></translation>
+        <translation>Особенности</translation>
     </message>
     <message>
         <source>Automatic ear detection</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматическое обнаружение подключения</translation>
     </message>
     <message>
         <source>Low audio latency</source>
-        <translation type="unfinished"></translation>
+        <translation>Низкая задержка звука</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open source &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;repository&lt;/span&gt;&lt;/a&gt; and licensed under &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;GPLv3&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Copyright © 2021-2022 SpriteOvO. All rights reserved.</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
