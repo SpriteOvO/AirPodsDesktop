abafd63f47cafae39b05fadeb4e3e91c8c5f3908
Peter Dave Hello <hsu@peterdavehello.org>
2023-09-22 18:47:25 +0800

    Add zh_TW translation

---
+++ Source/Resource/Translation/apd_zh_TW.ts
@@ -0,0 +1,224 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>ApdApplication</name>
+    <message>
+        <source>Settings format has changed a bit and needs to be reconfigured.</source>
+        <translation>設定格式有些許變動，需要重新設定。</translation>
+    </message>
+    <message>
+        <source>Hello, welcome to %1!
+
+This seems to be your first time using this program.
+Let&apos;s configure something together.</source>
+        <translation>您好，歡迎使用 %1！
+
+這似乎是您第一次使用本軟體。
+讓我們一同進行初次設定吧！</translation>
+    </message>
+    <message>
+        <source>Do you want this program to launch when the system starts?
+
+If you frequently use AirPods with this computer, it is recommended that you click &quot;Yes&quot;.</source>
+        <translation>您想讓這個軟體在開機時啟動嗎？
+
+如果您經常與這臺電腦一起使用 AirPods，建議您點選「是」。</translation>
+    </message>
+    <message>
+        <source>Do you want to enable the &quot;low audio latency&quot; feature?
+
+%1</source>
+        <translation>您想要啟用「低音訊延遲」功能嗎？
+
+%1</translation>
+    </message>
+    <message>
+        <source>Great! Everything is ready!
+
+Enjoy it all.</source>
+        <translation>太好了！一切都準備好了！
+
+盡情享受吧。</translation>
+    </message>
+    <message>
+        <source>You can find me in the system tray</source>
+        <translation>您可以在系統列中找到我</translation>
+    </message>
+    <message>
+        <source>Click the icon to view battery information, right-click to customize settings or quit.</source>
+        <translation>點選圖示以檢視電池資訊，右鍵點選以自訂設定或退出。</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadWindow</name>
+    <message>
+        <source>Download new version</source>
+        <translation>下載新版本</translation>
+    </message>
+    <message>
+        <source>Download Manually</source>
+        <translation>手動下載</translation>
+    </message>
+    <message>
+        <source>If the download is slow or fails, you can:</source>
+        <translation>如果下載緩慢或失敗，您可以：</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::DownloadWindow</name>
+    <message>
+        <source>Oops, an error occurred during the automatic update.
+Please download and install the new version manually.</source>
+        <translation>哎呀，在自動更新過程中發生錯誤。
+請手動下載並安裝新版本。</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::MainWindow</name>
+    <message>
+        <source>Change log:</source>
+        <translation>更新日誌：</translation>
+    </message>
+    <message>
+        <source>Hey! I found a new version available!
+
+Current version: %1
+Latest version: %2%3</source>
+        <translation>嗨！我發現有新版本可用！
+
+目前版本：%1
+最新版本：%2%3</translation>
+    </message>
+    <message>
+        <source>Bind to AirPods</source>
+        <translation>綁定至 AirPods</translation>
+    </message>
+    <message>
+        <source>Please select your AirPods device below.</source>
+        <translation>請在下方選擇您的 AirPods 裝置。</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::SettingsWindow</name>
+    <message>
+        <source>|</source>
+        <extracomment>To credit translators, you can leave your name here if you wish. (Sorted by time added, separated by &quot;|&quot; character, only single &quot;|&quot; for empty)</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Translation Contributors:</source>
+        <translation>翻譯貢獻者：</translation>
+    </message>
+    <message>
+        <source>Third-Party Libraries:</source>
+        <translation>第三方函式庫：</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::TrayIcon</name>
+    <message>
+        <source>Left</source>
+        <translation>左</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>右</translation>
+    </message>
+    <message>
+        <source>Case</source>
+        <translation>充電盒</translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <translation>充電中</translation>
+    </message>
+    <message>
+        <source>New version available!</source>
+        <translation>有新版本可用！</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>AirPodsDesktop information</source>
+        <translation>AirPodsDesktop 資訊</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>No paired device found.
+You need to pair your AirPods in Windows Bluetooth Settings first.</source>
+        <translation>未找到已配對的裝置。
+您需要先在 Windows 藍牙設定中配對您的 AirPods。</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>一般</translation>
+    </message>
+    <message>
+        <source>Run at startup</source>
+        <translation>開機時執行</translation>
+    </message>
+    <message>
+        <source>Enable low audio latency</source>
+        <translation>啟用低音訊延遲</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>進階</translation>
+    </message>
+    <message>
+        <source>Enable debug mode</source>
+        <translation>啟用偵錯模式</translation>
+    </message>
+    <message>
+        <source>Check for updates</source>
+        <translation>檢查更新</translation>
+    </message>
+    <message>
+        <source>Check Now</source>
+        <translation>立即檢查</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <source>Version: %1</source>
+        <translation>版本：%1</translation>
+    </message>
+    <message>
+        <source>License: %1</source>
+        <translation>授權：%1</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>贊助</translation>
+    </message>
+    <message>
+        <source>Donate to the developer</source>
+        <translation>贊助開發者</translation>
+    </message>
+</context>
+</TS>
