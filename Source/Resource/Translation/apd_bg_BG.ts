1791ec9487f958b1b2ee79904f6976fb04738aa9
m-chavalinov <112782488+m-chavalinov@users.noreply.github.com>
2025-12-11 15:38:54 +0200

    Add Bulgarian translations for AirPodsDesktop
    This file contains Bulgarian translations for various messages in the AirPodsDesktop application, including settings, notifications, and error messages.

---
+++ Source/Resource/Translation/apd_bg_BG.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg_BG">
+<context>
+    <name>ApdApplication</name>
+    <message>
+        <source>Settings format has changed a bit and needs to be reconfigured.</source>
+        <translation>Форматът на настройките е малко променен и трябва да бъде преконфигуриран.</translation>
+    </message>
+    <message>
+        <source>Hello, welcome to %1!
+
+This seems to be your first time using this program.
+Let&apos;s configure something together.</source>
+        <translation>Здравейте, добре дошли в %1!
+
+Изглежда, че това е първият ви път с тази програма.
+Нека конфигурираме нещо заедно.</translation>
+    </message>
+    <message>
+        <source>Do you want this program to launch when the system starts?
+
+If you frequently use AirPods with this computer, it is recommended that you click &quot;Yes&quot;.</source>
+        <translation>Искате ли тази програма да се стартира при включване на системата?
+
+Ако често използвате AirPods с този компютър, препоръчва се да натиснете „Да“.</translation>
+    </message>
+    <message>
+        <source>Do you want to enable the &quot;low audio latency&quot; feature?
+
+%1</source>
+        <translation>Искате ли да активирате функцията „ниска аудио латентност“?
+
+%1</translation>
+    </message>
+    <message>
+        <source>Great! Everything is ready!
+
+Enjoy it all.</source>
+        <translation>Страхотно! Всичко е готово!
+
+Наслаждавайте се.</translation>
+    </message>
+    <message>
+        <source>You can find me in the system tray</source>
+        <translation>Можете да ме намерите в системната област</translation>
+    </message>
+    <message>
+        <source>Click the icon to view battery information, right-click to customize settings or quit.</source>
+        <translation>Кликнете върху иконата, за да видите информацията за батерията, а с десен бутон – за настройки или изход.</translation>
+    </message>
+</context>
+
+<context>
+    <name>DownloadWindow</name>
+    <message>
+        <source>Download new version</source>
+        <translation>Изтегляне на новата версия</translation>
+    </message>
+    <message>
+        <source>Download Manually</source>
+        <translation>Ръчно изтегляне</translation>
+    </message>
+    <message>
+        <source>If the download is slow or fails, you can:</source>
+        <translation>Ако изтеглянето е бавно или се провали, можете да:</translation>
+    </message>
+</context>
+
+<context>
+    <name>Gui::DownloadWindow</name>
+    <message>
+        <source>Oops, an error occurred during the automatic update.
+Please download and install the new version manually.</source>
+        <translation>Опа, възникна грешка по време на автоматичната актуализация.
+Моля, изтеглете и инсталирайте новата версия ръчно.</translation>
+    </message>
+</context>
+
+<context>
+    <name>Gui::MainWindow</name>
+    <message>
+        <source>Change log:</source>
+        <translation>Дневник на промените:</translation>
+    </message>
+    <message>
+        <source>Hey! I found a new version available!
+
+Current version: %1
+Latest version: %2%3</source>
+        <translation>Хей! Намерих налична нова версия!
+
+Текуща версия: %1
+Последна версия: %2%3</translation>
+    </message>
+    <message>
+        <source>Bind to AirPods</source>
+        <translation>Свързване с AirPods</translation>
+    </message>
+    <message>
+        <source>Please select your AirPods device below.</source>
+        <translation>Моля, изберете вашето AirPods устройство по-долу.</translation>
+    </message>
+</context>
+
+<context>
+    <name>Gui::SettingsWindow</name>
+    <message>
+        <source>|</source>
+        <extracomment>To credit translators...</extracomment>
+        <translation>Oleh Hnat|</translation>
+    </message>
+    <message>
+        <source>Translation Contributors:</source>
+        <translation>Сътрудници в превода:</translation>
+    </message>
+    <message>
+        <source>Third-Party Libraries:</source>
+        <translation>Библиотеки на трети страни:</translation>
+    </message>
+</context>
+
+<context>
+    <name>Gui::TrayIcon</name>
+    <message>
+        <source>Left</source>
+        <translation>Ляво</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Дясно</translation>
+    </message>
+    <message>
+        <source>Case</source>
+        <translation>Кутия</translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <translation>зарежда се</translation>
+    </message>
+    <message>
+        <source>New version available!</source>
+        <translation>Налична е нова версия!</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Относно</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Изход</translation>
+    </message>
+</context>
+
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>AirPodsDesktop information</source>
+        <translation>Информация за AirPodsDesktop</translation>
+    </message>
+</context>
+
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>Update now</source>
+        <translation>Актуализирай сега</translation>
+    </message>
+    <message>
+        <source>Skip this version</source>
+        <translation>Пропусни тази версия</translation>
+    </message>
+    <message>
+        <source>View release</source>
+        <translation>Виж изданието</translation>
+    </message>
+    <message>
+        <source>Remind me later</source>
+        <translation>Напомни ми по-късно</translation>
+    </message>
+    <message>
+        <source>No paired device found.
+You need to pair your AirPods in Windows Bluetooth Settings first.</source>
+        <translation>Не е намерено сдвоено устройство.
+Първо трябва да сдвоите вашите AirPods в настройките на Bluetooth в Windows.</translation>
+    </message>
+</context>
+
+<context>
+    <name>QObject</name>
+    <message>
+        <source>It fixes short audio playback problems, but may increase battery consumption.</source>
+        <translation>Това поправя проблеми с краткото аудио възпроизвеждане, но може да увеличи разхода на батерията.</translation>
+    </message>
+    <message>
+        <source>It automatically pauses or resumes media when your AirPods are taken out or put in your ears.</source>
+        <translation>Автоматично поставя на пауза или възобновява медиите, когато AirPods се извадят или поставят в ушите.</translation>
+    </message>
+    <message>
+        <source>Unavailable</source>
+        <translation>Недостъпно</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Изключено</translation>
+    </message>
+    <message>
+        <source>Waiting for Binding</source>
+        <translation>Изчакване за свързване</translation>
+    </message>
+</context>
+
+<context>
+    <name>SelectWindow</name>
+    <message>
+        <source>Select</source>
+        <translation>Избери</translation>
+    </message>
+</context>
+
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>AirPodsDesktop Settings</source>
+        <translation>Настройки на AirPodsDesktop</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Общи</translation>
+    </message>
+    <message>
+        <source>Open logs directory</source>
+        <translation>Отваряне на директорията с логове</translation>
+    </message>
+    <message>
+        <source>Bluetooth maximum receiving range</source>
+        <translation>Максимален обхват на Bluetooth приемане</translation>
+    </message>
+    <message>
+        <source>Unbind AirPods</source>
+        <translation>Разкачване на AirPods</translation>
+    </message>
+    <message>
+        <source>Launch when system starts</source>
+        <translation>Стартирай при включване на системата</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Език</translation>
+    </message>
+    <message>
+        <source>Visual</source>
+        <translation>Визуално</translation>
+    </message>
+    <message>
+        <source>Display battery on taskbar</source>
+        <translation>Показване на батерията в лентата на задачите</translation>
+    </message>
+    <message>
+        <source>Disable</source>
+        <translation>Деактивирай</translation>
+    </message>
+    <message>
+        <source>When low battery</source>
+        <translation>При ниска батерия</translation>
+    </message>
+    <message>
+        <source>Always</source>
+        <translation>Винаги</translation>
+    </message>
+    <message>
+        <source>As text</source>
+        <translation>Като текст</translation>
+    </message>
+    <message>
+        <source>As icon</source>
+        <translation>Като икона</translation>
+    </message>
+    <message>
+        <source>Display battery on tray icon</source>
+        <translation>Показване на батерията върху иконата в системната област</translation>
+    </message>
+    <message>
+        <source>Features</source>
+        <translation>Функции</translation>
+    </message>
+    <message>
+        <source>Automatic ear detection</source>
+        <translation>Автоматично разпознаване на ухото</translation>
+    </message>
+    <message>
+        <source>Low audio latency</source>
+        <translation>Ниско закъснение на звука</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Относно</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open source &lt;a href="https://github.com/SpriteOvO/AirPodsDesktop"&gt;&lt;span style=" text-decoration: underline; color:#0000ff;"&gt;repository&lt;/span&gt;&lt;/a&gt; and licensed under &lt;a href="https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE"&gt;&lt;span style=" text-decoration: underline; color:#0000ff;"&gt;GPLv3&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Отворен код, &lt;a href="https://github.com/SpriteOvO/AirPodsDesktop"&gt;&lt;span style=" text-decoration: underline; color:#0000ff;"&gt;репозитория&lt;/span&gt;&lt;/a&gt; и лицензирано под &lt;a href="https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE"&gt;&lt;span style=" text-decoration: underline; color:#0000ff;"&gt;GPLv3&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Copyright © 2021-2022 SpriteOvO. All rights reserved.</source>
+        <translation>Авторско право © 2021–2022 SpriteOvO. Всички права запазени.</translation>
+    </message>
+</context>
+</TS>
