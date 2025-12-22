21ca6ba83f6c6cbc615ee630a051362fb0dadb15
Kaan HAS <kaanhas.52@gmail.com>
2025-12-22 21:44:33 +0300

    Add Turkish translation support (tr_TR) to the application.

---
+++ Source/Resource/Translation/apd_tr_TR.ts
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>ApdApplication</name>
+    <message>
+        <source>Settings format has changed a bit and needs to be reconfigured.</source>
+        <translation>Ayar formatı biraz değişti ve yeniden yapılandırılması gerekiyor.</translation>
+    </message>
+    <message>
+        <source>Hello, welcome to %1!
+
+This seems to be your first time using this program.
+Let&apos;s configure something together.</source>
+        <translation>Merhaba, %1&apos;e hoş geldiniz!
+
+Görünüşe göre bu programı ilk kez kullanıyorsunuz.
+Hadi birlikte bazı ayarları yapalım.</translation>
+    </message>
+    <message>
+        <source>Do you want this program to launch when the system starts?
+
+If you frequently use AirPods with this computer, it is recommended that you click &quot;Yes&quot;.</source>
+        <translation>Bu programın sistem başladığında açılmasını ister misiniz?
+
+AirPods&apos;u bu bilgisayarla sık kullanıyorsanız &quot;Evet&quot;e tıklamanız önerilir.</translation>
+    </message>
+    <message>
+        <source>Do you want to enable the &quot;low audio latency&quot; feature?
+
+%1</source>
+        <translation>&quot;Düşük ses gecikmesi&quot; özelliğini etkinleştirmek ister misiniz?
+
+%1</translation>
+    </message>
+    <message>
+        <source>Great! Everything is ready!
+
+Enjoy it all.</source>
+        <translation>Harika! Her şey hazır!
+
+Keyfini çıkarın.</translation>
+    </message>
+    <message>
+        <source>You can find me in the system tray</source>
+        <translation>Beni sistem tepsisinde bulabilirsiniz</translation>
+    </message>
+    <message>
+        <source>Click the icon to view battery information, right-click to customize settings or quit.</source>
+        <translation>Pil bilgilerini görmek için simgeye tıklayın, ayarları özelleştirmek veya çıkmak için sağ tıklayın.</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadWindow</name>
+    <message>
+        <source>Download new version</source>
+        <translation>Yeni sürümü indir</translation>
+    </message>
+    <message>
+        <source>Download Manually</source>
+        <translation>Manuel olarak indir</translation>
+    </message>
+    <message>
+        <source>If the download is slow or fails, you can:</source>
+        <translation>İndirme yavaşsa veya başarısız olursa şunları yapabilirsiniz:</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::DownloadWindow</name>
+    <message>
+        <source>Oops, an error occurred during the automatic update.
+Please download and install the new version manually.</source>
+        <translation>Ops, otomatik güncelleme sırasında bir hata oluştu.
+Lütfen yeni sürümü manuel olarak indirip yükleyin.</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::MainWindow</name>
+    <message>
+        <source>Change log:</source>
+        <translation>Değişiklik günlüğü:</translation>
+    </message>
+    <message>
+        <source>Hey! I found a new version available!
+
+Current version: %1
+Latest version: %2%3</source>
+        <translation>Hey! Yeni bir sürüm mevcut!
+
+Mevcut sürüm: %1
+En son sürüm: %2%3</translation>
+    </message>
+    <message>
+        <source>Bind to AirPods</source>
+        <translation>AirPods ile Eşleştir</translation>
+    </message>
+    <message>
+        <source>Please select your AirPods device below.</source>
+        <translation>Lütfen aşağıdan AirPods cihazınızı seçin.</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::SettingsWindow</name>
+    <message>
+        <source>|</source>
+        <extracomment>To credit translators, you can leave your name here if you wish. (Sorted by time added, separated by &quot;|&quot; character, only single &quot;|&quot; for empty)</extracomment>
+        <translation>Kaan HAS</translation>
+    </message>
+    <message>
+        <source>Translation Contributors:</source>
+        <translation>Çeviriye Katkıda Bulunanlar:</translation>
+    </message>
+    <message>
+        <source>Third-Party Libraries:</source>
+        <translation>Üçüncü Taraf Kütüphaneler:</translation>
+    </message>
+</context>
+<context>
+    <name>Gui::TrayIcon</name>
+    <message>
+        <source>Left</source>
+        <translation>Sol</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Sağ</translation>
+    </message>
+    <message>
+        <source>Case</source>
+        <translation>Kutu</translation>
+    </message>
+    <message>
+        <source>charging</source>
+        <translation>şarj oluyor</translation>
+    </message>
+    <message>
+        <source>New version available!</source>
+        <translation>Yeni sürüm mevcut!</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Çıkış</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>AirPodsDesktop information</source>
+        <translation>AirPodsDesktop bilgileri</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <source>Update now</source>
+        <translation>Şimdi güncelle</translation>
+    </message>
+    <message>
+        <source>Skip this version</source>
+        <translation>Bu sürümü atla</translation>
+    </message>
+    <message>
+        <source>View release</source>
+        <translation>Sürümü görüntüle</translation>
+    </message>
+    <message>
+        <source>Remind me later</source>
+        <translation>Daha sonra hatırlat</translation>
+    </message>
+    <message>
+        <source>No paired device found.
+You need to pair your AirPods in Windows Bluetooth Settings first.</source>
+        <translation>Eşleşmiş cihaz bulunamadı.
+Önce Windows Bluetooth Ayarları&apos;ndan AirPods&apos;unuzu eşleştirmeniz gerekir.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>It fixes short audio playback problems, but may increase battery consumption.</source>
+        <translation>Kısa ses çalma sorunlarını düzeltir, ancak pil tüketimini artırabilir.</translation>
+    </message>
+    <message>
+        <source>It automatically pauses or resumes media when your AirPods are taken out or put in your ears.</source>
+        <translation>AirPods&apos;unuzu çıkardığınızda veya kulağınıza taktığınızda medyayı otomatik olarak duraklatır veya devam ettirir.</translation>
+    </message>
+    <message>
+        <source>Unavailable</source>
+        <translation>Kullanılamıyor</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Bağlantı Kesildi</translation>
+    </message>
+    <message>
+        <source>Waiting for Binding</source>
+        <translation>Eşleştirme Bekleniyor</translation>
+    </message>
+</context>
+<context>
+    <name>SelectWindow</name>
+    <message>
+        <source>Select</source>
+        <translation>Seç</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>AirPodsDesktop Settings</source>
+        <translation>AirPodsDesktop Ayarları</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Genel</translation>
+    </message>
+    <message>
+        <source>Open logs directory</source>
+        <translation>Log klasörünü aç</translation>
+    </message>
+    <message>
+        <source>Bluetooth maximum receiving range</source>
+        <translation>Maksimum Bluetooth kapsama alanı</translation>
+    </message>
+    <message>
+        <source>Unbind AirPods</source>
+        <translation>AirPods Bağlantısını Kes</translation>
+    </message>
+    <message>
+        <source>Launch when system starts</source>
+        <translation>Sistem başlangıcında çalıştır</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Dil</translation>
+    </message>
+    <message>
+        <source>Visual</source>
+        <translation>Görünüm</translation>
+    </message>
+    <message>
+        <source>Display battery on tray icon</source>
+        <translation>Sistem tepsisi simgesinde pili göster</translation>
+    </message>
+    <message>
+        <source>Disable</source>
+        <translation>Devre Dışı</translation>
+    </message>
+    <message>
+        <source>When low battery</source>
+        <translation>Pil azaldığında</translation>
+    </message>
+    <message>
+        <source>Always</source>
+        <translation>Her zaman</translation>
+    </message>
+    <message>
+        <source>Features</source>
+        <translation>Özellikler</translation>
+    </message>
+    <message>
+        <source>Automatic ear detection</source>
+        <translation>Otomatik kulak algılama</translation>
+    </message>
+    <message>
+        <source>Low audio latency</source>
+        <translation>Düşük ses gecikmesi</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open source &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;repository&lt;/span&gt;&lt;/a&gt; and licensed under &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;GPLv3&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Açık kaynaklı &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;depo&lt;/span&gt;&lt;/a&gt; ve &lt;a href=&quot;https://github.com/SpriteOvO/AirPodsDesktop/blob/main/LICENSE&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;GPLv3&lt;/span&gt;&lt;/a&gt; altında lisanslanmıştır.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Copyright © 2021-2022 SpriteOvO. All rights reserved.</source>
+        <translation>Telif Hakkı © 2021-2022 SpriteOvO. Tüm hakları saklıdır.</translation>
+    </message>
+    <message>
+        <source>Display battery on taskbar</source>
+        <translation>Görev çubuğunda pili göster</translation>
+    </message>
+    <message>
+        <source>As text</source>
+        <translation>Metin olarak</translation>
+    </message>
+    <message>
+        <source>As icon</source>
+        <translation>Simge olarak</translation>
+    </message>
+</context>
+</TS>
